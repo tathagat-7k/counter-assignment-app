70278e35ee93066f299828ba5f452c27a743ee82
tathagat-7k <tathagat@getdefault.in>
2023-02-22 23:02:25 +0530

    Made a counter app

---
+++ src/Counter.js
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from "react";
+
+const ONE_SEC = 1000;
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const [showPauseButton, setShowPauseButton] = useState(false);
+
+  const counterInterval = useRef();
+
+  const startCounting = () => {
+    setShowPauseButton(true);
+    counterInterval.current = setInterval(() => {
+      setCounter((prev) => prev + 1);
+    }, ONE_SEC);
+  };
+
+  const stopCounting = () => {
+    clearInterval(counterInterval.current);
+  };
+
+  const resetCounter = () => {
+    clearInterval(counterInterval.current);
+    setCounter(0);
+    setShowPauseButton(false);
+  };
+
+  return (
+    <div>
+      <h1 className="counter-number">{counter}</h1>
+      <div className="action-button-container">
+        {!showPauseButton ? (
+          <button onClick={startCounting}>Start</button>
+        ) : (
+          <>
+            <button onClick={stopCounting}>Pause</button>
+            <button onClick={resetCounter}>Reset</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
